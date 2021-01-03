b0a9484a9c74fe8ee672522752c329e11ea94d50
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2021-01-03 21:28:41 +0100

    poprawki

--- main.c
+++ main.c
@@ -15,91 +15,31 @@ struct eqData
     GtkWidget* equation;
     GtkWidget* intervalL;
     GtkWidget* intervalP;
+    GtkWidget* res;
 };
 
 GdkPixbuf* chartData;
 GtkWidget* chart;
 
+static void put_lines_to_chart(GdkPixbuf* pixbuf, gdouble l, gdouble p);
+static void destroy (GtkWidget *widget, gpointer data);
+static void about_run (GtkWidget *widget, gpointer data);
+static void put_pixel (GdkPixbuf* pixbuf, int x, int y, guchar red, guchar green, guchar blue, guchar alpha);
+static void calculate (GtkWidget *widget, gpointer data_);
+
 void scan_statement(char* statement);
-
-static void destroy (GtkWidget *widget, gpointer data) {
-  gtk_main_quit();
-}
-
-static void put_pixel (GdkPixbuf* pixbuf, int x, int y, guchar red, guchar green, guchar blue, guchar alpha)
-{
-    int width, height, rowstride, n_channels;
-    guchar *pixels, *p;
-
-    n_channels = gdk_pixbuf_get_n_channels (pixbuf);
-
-    g_assert (gdk_pixbuf_get_colorspace(pixbuf) == GDK_COLORSPACE_RGB);
-    g_assert (gdk_pixbuf_get_bits_per_sample(pixbuf) == 8);
-    g_assert (gdk_pixbuf_get_has_alpha (pixbuf));
-    g_assert (n_channels == 4);
-
-    width = gdk_pixbuf_get_width (pixbuf);
-    height = gdk_pixbuf_get_height (pixbuf);
-
-    if (x < 0 || x >= width) {g_print("No nie %d x\n", x); return;}
-    if (y < 0 || y >= height) {g_print("No nie %d y\n", y); return;}
-
-    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
-    pixels = gdk_pixbuf_get_pixels(pixbuf);
-
-    p = pixels + y * rowstride + x * n_channels;
-    p[0] = red;
-    p[1] = green;
-    p[2] = blue;
-    p[3] = alpha;
-}
-
-static void calculate (GtkWidget *widget, gpointer data_) 
-{
-    struct eqData* data = (struct eqData*) data_;
-    
-    const gchar* wejscie, *intervalL, *intervalP;
-    gchar* eptr;
-    gdouble l, p, delta;
-    gint r;
-
-    wejscie = gtk_entry_get_text(GTK_ENTRY(data->equation));
-    intervalL = gtk_entry_get_text(GTK_ENTRY(data->intervalL));
-    intervalP = gtk_entry_get_text(GTK_ENTRY(data->intervalP));
-
-    l = strtod(intervalL, &eptr); p = strtod(intervalP, &eptr);
-    r = (gint)800;
-    delta = (p - l) / (gdouble)r;
-
-    //gdouble wyniki[r + 1];
-
-    for (gint i = 0; i < r; i++)
-    {
-        bool dziedzina = true;
-        double wynik = calc(wejscie, strlen(wejscie), l, &dziedzina);
-
-        //wyniki[i] = wynik;
-        if (dziedzina) put_pixel(chartData, i, 300 - wynik, 0, 0, 255, 255);
-        //if (dziedzina) g_print("f(%lf) = %lf\n", l, wynik);
-        //else g_print("f(%lf) = \"Podza dziedzina!\"\n", l);
-        l+=delta;
-    }
-
-    gtk_image_set_from_pixbuf(GTK_IMAGE(chart), chartData);
-
-    //g_print("%lf\n", calc(wejscie, strlen(wejscie), 1.0));
-}
 
 int main(int argc, char* argv[])
 {
     gtk_init(&argc, &argv);
 
-    GtkWidget* window, *vbox, *menubar, *button;
+    GtkWidget *window, *vbox, *menubar, *button;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
 
     gtk_window_set_default_size(GTK_WINDOW(window), (gint)(600), (gint)200);
+    gtk_window_set_title(GTK_WINDOW(window), "Wykresy");
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 
     button = gtk_button_new_with_label("Wprowadź");
@@ -120,6 +60,8 @@ int main(int argc, char* argv[])
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(file_item), file_menu);
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), quit_item);
 
+    g_signal_connect(G_OBJECT(quit_item), "activate", G_CALLBACK(destroy), NULL);
+
     file_item = gtk_menu_item_new_with_label("Pomoc");
     file_menu = gtk_menu_new();
     quit_item = gtk_menu_item_new_with_label("Wykresy - informacje");
@@ -128,7 +70,7 @@ int main(int argc, char* argv[])
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(file_item), file_menu);
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), quit_item);
 
-    g_signal_connect(G_OBJECT(quit_item), "activate", G_CALLBACK(destroy), NULL);
+    g_signal_connect(G_OBJECT(quit_item), "activate", G_CALLBACK(about_run), NULL);
 
     gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
 
@@ -136,15 +78,16 @@ int main(int argc, char* argv[])
     gtk_container_set_border_width(GTK_CONTAINER(box1), 10);
 
     chartData = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 800, 600);
+    put_lines_to_chart(chartData, 0, 0);
     chart = gtk_image_new_from_pixbuf(chartData);
 
     struct eqData inputs;
 
-    inputs.equation = gtk_entry_new(); inputs.intervalL = gtk_entry_new(); inputs.intervalP = gtk_entry_new(); //inputs.res = gtk_entry_new();
+    inputs.equation = gtk_entry_new(); inputs.intervalL = gtk_entry_new(); inputs.intervalP = gtk_entry_new(); inputs.res = gtk_entry_new();
     gtk_entry_set_max_length(GTK_ENTRY(inputs.equation), (gint)LENGTH);
     gtk_entry_set_max_length(GTK_ENTRY(inputs.intervalL), (gint)50);
     gtk_entry_set_max_length(GTK_ENTRY(inputs.intervalP), (gint)50);
-    //gtk_entry_set_max_length(GTK_ENTRY(inputs.res), (gint)50);
+    gtk_entry_set_max_length(GTK_ENTRY(inputs.res), (gint)50);
 
     g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(calculate), (gpointer)&inputs);
 
@@ -152,7 +95,7 @@ int main(int argc, char* argv[])
     gtk_box_pack_start(GTK_BOX(box1), inputs.equation, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(box1), inputs.intervalL, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(box1), inputs.intervalP, TRUE, TRUE, 0);
-    //gtk_box_pack_start(GTK_BOX(box1), inputs.res, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box1), inputs.res, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), box1, TRUE, TRUE, 0);
 
@@ -166,6 +109,113 @@ int main(int argc, char* argv[])
     printf("%lf", calc(str, strlen(str), 1.0));
 
     return 0;*/
+}
+
+static void put_lines_to_chart(GdkPixbuf* pixbuf, gdouble l, gdouble p)
+{
+    for (gint i = 0; i < 800; i++)
+    {
+        put_pixel(pixbuf, i, 299, 0, 0, 0, 255);
+        put_pixel(pixbuf, i, 300, 0, 0, 0, 255);
+    }
+    //temp
+
+    for (gint i = 0; i < 600; i++)
+    {
+        put_pixel(pixbuf, 399, i, 0, 0, 0, 255);
+        put_pixel(pixbuf, 400, i, 0, 0, 0, 255);
+    }
+}
+
+static void destroy (GtkWidget *widget, gpointer data) 
+{
+    gtk_main_quit();
+}
+
+static void about_run (GtkWidget *widget, gpointer data)
+{
+    GtkWidget* about = gtk_about_dialog_new();
+    gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(about), "Wykresy");
+    gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(about), "0.1");
+    gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about), "c Wiktor Ogrodnik 2021");
+    gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(about), GTK_LICENSE_MIT_X11);
+
+    gtk_dialog_run(GTK_DIALOG(about));
+    gtk_widget_destroy(about);
+}
+
+static void put_pixel (GdkPixbuf* pixbuf, int x, int y, guchar red, guchar green, guchar blue, guchar alpha)
+{
+    int width, height, rowstride, n_channels;
+    guchar *pixels, *p;
+
+    n_channels = gdk_pixbuf_get_n_channels (pixbuf);
+
+    g_assert (gdk_pixbuf_get_colorspace(pixbuf) == GDK_COLORSPACE_RGB);
+    g_assert (gdk_pixbuf_get_bits_per_sample(pixbuf) == 8);
+    g_assert (gdk_pixbuf_get_has_alpha (pixbuf));
+    g_assert (n_channels == 4);
+
+    width = gdk_pixbuf_get_width (pixbuf);
+    height = gdk_pixbuf_get_height (pixbuf);
+
+    if (x < 0 || x >= width) return;
+    if (y < 0 || y >= height) return;
+
+    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+    pixels = gdk_pixbuf_get_pixels(pixbuf);
+
+    p = pixels + y * rowstride + x * n_channels;
+    p[0] = red;
+    p[1] = green;
+    p[2] = blue;
+    p[3] = alpha;
+}
+
+static void calculate (GtkWidget *widget, gpointer data_) 
+{
+    struct eqData* data = (struct eqData*) data_;
+    
+    const gchar* wejscie, *intervalL, *intervalP, *scale_;
+    gchar* eptr;
+    gdouble l, p, delta, scale;
+    gint r;
+
+    wejscie = gtk_entry_get_text(GTK_ENTRY(data->equation));
+    intervalL = gtk_entry_get_text(GTK_ENTRY(data->intervalL));
+    intervalP = gtk_entry_get_text(GTK_ENTRY(data->intervalP));
+    scale_ = gtk_entry_get_text(GTK_ENTRY(data->res));
+
+    l = strtod(intervalL, &eptr); p = strtod(intervalP, &eptr); scale = strtod(scale_, &eptr);
+    r = (gint)800;
+    delta = (p - l) / (gdouble)r;
+
+    gdouble wyniki[r];
+    gdouble max = -800.0;
+
+    for (gint i = 0; i < r; i++)
+    {
+        bool dziedzina = true;
+        double wynik = calc(wejscie, strlen(wejscie), l, &dziedzina);
+
+        if (dziedzina) wyniki[i] = wynik;
+        else wyniki[i] = 800.0;
+
+        if (dziedzina && wynik > max) max = wynik;
+        l+=delta;
+    }
+
+    chartData = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 800, 600);
+    put_lines_to_chart(chartData, l, p);
+
+    max += max * 0.4;
+    if (max > 300) max = 300;
+
+    for (gint i = 0; i < r; i++) put_pixel(chartData, i, (gint)(300 - (wyniki[i]) * scale), 0, 0, 255, 255);
+    
+    gtk_image_set_from_pixbuf(GTK_IMAGE(chart), chartData);
+
+    //g_print("%lf\n", calc(wejscie, strlen(wejscie), 1.0));
 }
 
 void scan_statement(char* statement)
