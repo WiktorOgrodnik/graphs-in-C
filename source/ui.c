d6d49bb5210ce4c508020aae3fcc887d635ed8dd
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2021-01-29 20:46:05 +0100

    -Dodano licencję z gita do programu

--- source/ui.c
+++ source/ui.c
@@ -260,11 +260,13 @@ static void destroy (GtkWidget* widget, gpointer data)
 
 static void about_run (GtkWidget* widget, gpointer data)
 {
+    const char license[] = "MIT License\n\nCopyright (c) 2021 Wiktor Ogrodnik\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n";
+
     GtkWidget* about = gtk_about_dialog_new();
     gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(about), "Wykresy");
     gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(about), "0.1");
     gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about), "c Wiktor Ogrodnik 2021");
-    gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(about), GTK_LICENSE_MIT_X11);
+    gtk_about_dialog_set_license(GTK_ABOUT_DIALOG(about), license);
 
     gtk_dialog_run(GTK_DIALOG(about));
     gtk_widget_destroy(about);
